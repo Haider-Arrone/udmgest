4c2ce01cdf6bdef67ea144428a48a4ee6fea186d
Haider Ibraimo <haider.arrone12@gmail.com>
2022-08-03 20:45:09 +0200

    paginacao a funcionar

--- expedient/views.py
+++ expedient/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
+
+from utils.expedient.pagination import make_pagination
 from .models import Expedient
+from django.core.paginator import Paginator
+
+PER_PAGES = 20
 
 def home(request):
     expedients = Expedient.objects.filter(recebido=False).order_by('-id')
     
-    #page_obj, pagination_range = make_pagination(request, recipes, PER_PAGES)
+    page_obj, pagination_range = make_pagination(request, expedients, PER_PAGES)
     
     return render(request, 'expedient/pages/home.html', context={
-        'expedients': expedients,
-    #    'pagination_range': pagination_range,
+        'expedients': page_obj,
+        'pagination_range': pagination_range,
        
+    })
+
+def expedient(request, id):
+    expedient = Expedient.objects.filter(id=id).order_by('-id').first
+    return render(request, 'expedient/pages/expedient-view.html', context={
+        'expedient': expedient(),
+        
     })
