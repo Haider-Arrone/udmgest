2e0171a369ada0bc81584d5230f36fb19ead088d
Haider-Arrone <haider.arrone12@gmail.com>
2025-01-15 13:49:12 +0200

    teste 2 cors

--- project/settings.py
+++ project/settings.py
@@ -29,7 +29,10 @@ ALLOWED_HOSTS: list[str] = ['*']
 CORS_ALLOW_ALL_ORIGINS = True
 CSRF_TRUSTED_ORIGINS = ['http://udmgest.udm.ac.mz', 'https://udmgest.udm.ac.mz', 'http://10.10.0.92', 'http://localhost', 'http://127.0.0.1','http://udmgest.udm.ac.mz:5050',  
     'https://udmgest.udm.ac.mz:5050',]
+CSRF_COOKIE_SECURE = False
+SESSION_COOKIE_SECURE = False
 
+CORS_ALLOW_CREDENTIALS = True
 # CORS_ALLOW_CREDENTIALS = True
 # CORS_EXPOSE_HEADERS = ['Content-Type', 'X-CSRFToken']
 
